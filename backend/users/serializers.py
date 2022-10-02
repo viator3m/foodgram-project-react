a256c3fb56760e61c76fb9213aa77d3af7c53a1c
viator3m <viator3m@gmail.com>
2022-10-02 11:25:50 +0300

    Realisation views and serializer of users

---
+++ backend/users/serializers.py
@@ -0,0 +1,46 @@
+from djoser.serializers import UserCreateSerializer, UserSerializer
+from rest_framework.exceptions import ValidationError
+from rest_framework.fields import SerializerMethodField
+
+from .models import User, Follow
+
+
+class UsersCreateSerializer(UserCreateSerializer):
+    class Meta:
+        model = User
+        fields = (
+            'email',
+            'id',
+            'username',
+            'first_name',
+            'last_name',
+            'password')
+
+        extra_kwargs = {"password": {"write_only": True}}
+
+    def validate_username(self, value):
+        if value == "me":
+            raise ValidationError(
+                'Невозможно создать пользователя с таким именем!'
+            )
+        return value
+
+
+class UsersSerializer(UserSerializer):
+    is_subscribed = SerializerMethodField(read_only=True)
+    class Meta:
+        model = User
+        fields = (
+            'email',
+            'id',
+            'username',
+            'first_name',
+            'last_name',
+            'is_subscribed'
+        )
+
+    def get_is_subscribed(self, object):
+        user = self.context.get('request').user
+        if user.is_anonymous:
+            return False
+        return Follow.objects.filter(user=user, author=object.id).exists()
--- backend/users/views.py
+++ backend/users/views.py
@@ -1,3 +1,13 @@
-from django.shortcuts import render
+from djoser.views import UserViewSet
+from rest_framework.permissions import IsAuthenticatedOrReadOnly
 
-# Create your views here.
+from .models import User
+from .serializers import UsersSerializer
+
+
+class UsersViewSet(UserViewSet):
+    queryset = User.objects.all()
+    serializer_class = UsersSerializer
+    permission_classes = (IsAuthenticatedOrReadOnly,)
+
+
