21227193305b025722876221a3302021ac6e724e
viator3m <viator3m@gmail.com>
2022-10-02 11:24:40 +0300

    Added custom model of user and model of follow

--- backend/users/models.py
+++ backend/users/models.py
@@ -1,6 +1,55 @@
 from django.contrib.auth.models import AbstractUser
+from django.core.validators import RegexValidator
+from django.db import models
 
 
 class User(AbstractUser):
-    def __init__(self, args, kwargs):
-        super().__init__(*args, **kwargs)
+    email = models.EmailField(
+        verbose_name='Электронная почта',
+        unique=True,
+        max_length=254
+    )
+    first_name = models.CharField(
+        verbose_name='Имя',
+        max_length=150,
+    )
+    last_name = models.CharField(
+        verbose_name='Фамилия',
+        max_length=150,
+    )
+
+    USERNAME_FIELD = 'email'
+    REQUIRED_FIELDS = ('username', 'first_name', 'last_name')
+
+    class Meta:
+        verbose_name = 'Пользователь'
+        verbose_name_plural = 'Пользователи'
+        constraints = [
+            models.UniqueConstraint(
+                fields=('username', 'email'),
+                name='unique_user'
+            )
+        ]
+
+
+class Follow(models.Model):
+    user = models.ForeignKey(
+        User,
+        related_name='follower',
+        on_delete=models.CASCADE
+    )
+    author = models.ForeignKey(
+        User,
+        related_name='following',
+        on_delete=models.CASCADE
+    )
+
+    def __str__(self):
+        return f'Автор: {self.author}, подписчик: {self.user}'
+
+    class Meta:
+        constraints = [
+            models.UniqueConstraint(
+                fields=['author', 'user'],
+                name='unique_follower')
+        ]
