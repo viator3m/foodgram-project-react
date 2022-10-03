90c59dfadc8c5840d7209af301229396f17ccb51
viator3m <viator3m@gmail.com>
2022-10-03 23:41:39 +0300

    Added management commands

---
+++ backend/recipes/management/commands/load_data.py
@@ -0,0 +1,27 @@
+import csv
+
+from foodgram import settings
+from django.core.management import BaseCommand
+from recipes.models import Ingredient, Tag
+from users.models import User
+
+MODELS_FILES = {
+    Ingredient: 'ingredients.csv',
+    Tag: 'tags.csv',
+    User: 'users.csv'
+}
+
+
+class Command(BaseCommand):
+    def handle(self, *args, **options):
+        for model, file in MODELS_FILES.items():
+            with open(
+                    f'{settings.BASE_DIR}/data/{file}',
+                    'r', encoding='utf-8',
+            ) as table:
+                reader = csv.DictReader(table)
+                model.objects.bulk_create(model(**data) for data in reader)
+
+        self.stdout.write(self.style.SUCCESS(
+            '=== Ингредиенты и теги успешно загружены ===')
+        )
---
+++ backend/recipes/management/commands/load_test_data.py
@@ -0,0 +1,26 @@
+import csv
+
+from foodgram import settings
+from django.core.management import BaseCommand
+from recipes.models import Recipe
+from users.models import User
+
+MODELS_FILES = {
+    User: 'users.csv',
+    Recipe: 'recipes.csv'
+}
+
+
+class Command(BaseCommand):
+    def handle(self, *args, **options):
+        for model, file in MODELS_FILES.items():
+            with open(
+                    f'{settings.BASE_DIR}/data/{file}',
+                    'r', encoding='utf-8',
+            ) as table:
+                reader = csv.DictReader(table)
+                model.objects.bulk_create(model(**data) for data in reader)
+
+        self.stdout.write(self.style.SUCCESS(
+            '=== Тестовые данные успешно загружены ===')
+        )
