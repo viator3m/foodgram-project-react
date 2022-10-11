b23721e93c26c7d6ba6c550ceaafbfe547c7c724
viator3m <viator3m@gmail.com>
2022-10-11 10:35:34 +0300

    Added migrations file

--- backend/foodgram/settings.py
+++ backend/foodgram/settings.py
@@ -8,7 +8,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
 SECRET_KEY = os.getenv('SECRET_KEY', default='your_secret_key')
 
-DEBUG = os.getenv('DEBUG', default=False)
+DEBUG = os.getenv('DEBUG', default=True)
 
 ALLOWED_HOSTS = [
     '127.0.0.1',
